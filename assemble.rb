fb7c76b76d9233363093dcfa86e0aceee54fbd44
Zachary Webb <zwebb@admins-mbp.ca.com>
2017-02-01 13:46:35 -0700

    Adding tables with descriptions to dashboard landing pages.

--- assemble.rb
+++ assemble.rb
@@ -1,5 +1,6 @@
-DASHBOARDS_PLACEHOLDER = "<!-- put the dashboards here -->"
 MENU_PLACEHOLDER = "<!-- put the menu here -->"
+DASHBOARDS_PLACEHOLDER = "// put the dashboards here"
+STYLE_PLACEHOLDER = "/* put the style here */"
 
 team_out = File.open("templates/team.html", "r").read
 program_out = File.open("templates/program.html", "r").read
@@ -20,10 +21,14 @@ program_menu = File.open("menus/program.html", "r").read
   .gsub("\"", "\\\"")
   .gsub("\n", " ")
 
+style = File.open("style.css", "r").read
+
 team_out.gsub!(DASHBOARDS_PLACEHOLDER, team_dashboards)
 team_out.gsub!(MENU_PLACEHOLDER, team_menu)
+team_out.gsub!(STYLE_PLACEHOLDER, style)
 program_out.gsub!(DASHBOARDS_PLACEHOLDER, program_dashboards)
 program_out.gsub!(MENU_PLACEHOLDER, program_menu)
+program_out.gsub!(STYLE_PLACEHOLDER, style)
 
 File.open("team_dashboard.html", "w") do |f|
   f << team_out
