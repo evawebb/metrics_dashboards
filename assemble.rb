099c5818a573783f65f5344fcc354eeb7d5c5f85
Zachary Webb <zwebb@admins-mbp.ca.com>
2017-02-01 12:59:10 -0700

    Separating menus out into their own files.

--- assemble.rb
+++ assemble.rb
@@ -1,3 +1,6 @@
+DASHBOARDS_PLACEHOLDER = "<!-- put the dashboards here -->"
+MENU_PLACEHOLDER = "<!-- put the menu here -->"
+
 team_out = File.open("team_dashboard_template.html", "r").read
 program_out = File.open("program_dashboard_template.html", "r").read
 
@@ -10,8 +13,17 @@ weekly = File.open("weekly_throughput_dashboard.js", "r").read
 team_dashboards = [all_work, team, weekly].join("\n")
 program_dashboards = [all_work, feature, initiative, weekly].join("\n")
 
-team_out.gsub!("<!-- put the things here -->", team_dashboards)
-program_out.gsub!("<!-- put the things here -->", program_dashboards)
+team_menu = File.open("team_dashboard_menu.html", "r").read
+  .gsub("\"", "\\\"")
+  .gsub("\n", " ")
+program_menu = File.open("program_dashboard_menu.html", "r").read
+  .gsub("\"", "\\\"")
+  .gsub("\n", " ")
+
+team_out.gsub!(DASHBOARDS_PLACEHOLDER, team_dashboards)
+team_out.gsub!(MENU_PLACEHOLDER, team_menu)
+program_out.gsub!(DASHBOARDS_PLACEHOLDER, program_dashboards)
+program_out.gsub!(MENU_PLACEHOLDER, program_menu)
 
 File.open("team_dashboard.html", "w") do |f|
   f << team_out
