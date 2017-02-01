84118464aaee09842bb945c5afdb618009d91c8f
Zachary Webb <zwebb@admins-mbp.ca.com>
2017-02-01 13:08:38 -0700

    Rearranging files.

--- assemble.rb
+++ assemble.rb
@@ -1,22 +1,22 @@
 DASHBOARDS_PLACEHOLDER = "<!-- put the dashboards here -->"
 MENU_PLACEHOLDER = "<!-- put the menu here -->"
 
-team_out = File.open("team_dashboard_template.html", "r").read
-program_out = File.open("program_dashboard_template.html", "r").read
+team_out = File.open("templates/team.html", "r").read
+program_out = File.open("templates/program.html", "r").read
 
-all_work = File.open("all_work_dashboard.js", "r").read
-feature = File.open("feature_dashboard.js", "r").read
-initiative = File.open("initiative_dashboard.js", "r").read
-team = File.open("team_dashboard.js", "r").read
-weekly = File.open("weekly_throughput_dashboard.js", "r").read
+all_work = File.open("dashboards/all_work.js", "r").read
+feature = File.open("dashboards/feature.js", "r").read
+initiative = File.open("dashboards/initiative.js", "r").read
+team = File.open("dashboards/team.js", "r").read
+weekly = File.open("dashboards/weekly_throughput.js", "r").read
 
 team_dashboards = [all_work, team, weekly].join("\n")
 program_dashboards = [all_work, feature, initiative, weekly].join("\n")
 
-team_menu = File.open("team_dashboard_menu.html", "r").read
+team_menu = File.open("menus/team.html", "r").read
   .gsub("\"", "\\\"")
   .gsub("\n", " ")
-program_menu = File.open("program_dashboard_menu.html", "r").read
+program_menu = File.open("menus/program.html", "r").read
   .gsub("\"", "\\\"")
   .gsub("\n", " ")
 
