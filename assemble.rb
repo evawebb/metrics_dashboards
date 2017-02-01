d57272814c34de3fafed07323128732299547e82
Zachary Webb <zwebb@admins-mbp.ca.com>
2017-02-01 14:20:52 -0700

    Add a page just for the team dashboard.

--- assemble.rb
+++ assemble.rb
@@ -4,6 +4,7 @@ STYLE_PLACEHOLDER = "/* put the style here */"
 
 team_out = File.open("templates/team.html", "r").read
 program_out = File.open("templates/program.html", "r").read
+just_team_out = File.open("templates/just_team.html", "r").read
 
 all_work = File.open("dashboards/all_work.js", "r").read
 feature = File.open("dashboards/feature.js", "r").read
@@ -23,16 +24,21 @@ program_menu = File.open("menus/program.html", "r").read
 
 style = File.open("style.css", "r").read
 
-team_out.gsub!(DASHBOARDS_PLACEHOLDER, team_dashboards)
 team_out.gsub!(MENU_PLACEHOLDER, team_menu)
 team_out.gsub!(STYLE_PLACEHOLDER, style)
-program_out.gsub!(DASHBOARDS_PLACEHOLDER, program_dashboards)
+team_out.gsub!(DASHBOARDS_PLACEHOLDER, team_dashboards)
 program_out.gsub!(MENU_PLACEHOLDER, program_menu)
 program_out.gsub!(STYLE_PLACEHOLDER, style)
+program_out.gsub!(DASHBOARDS_PLACEHOLDER, program_dashboards)
+just_team_out.gsub!(STYLE_PLACEHOLDER, style)
+just_team_out.gsub!(DASHBOARDS_PLACEHOLDER, team)
 
 File.open("team_dashboard.html", "w") do |f|
   f << team_out
 end
 File.open("program_dashboard.html", "w") do |f|
   f << program_out
+end
+File.open("just_team_dashboard.html", "w") do |f|
+  f << just_team_out
 end
